17b49c988ab362654a4d2d23d0967317f1afbb0a
Stanislav Nedelchev <stan@stannedelchev.net>
2015-08-27 21:15:09 +0300

    add ChatViewController to hold chat messages and message input
    * add segue from EditNameViewController to ChatViewController
    * use JSQMessagesViewController as base class to ChatViewController
    * disable segue if name is empty

---
+++ demo-ios/ChatViewController.swift
@@ -0,0 +1,13 @@
+//
+//  ChatViewController.swift
+//  demo-ios
+//
+//  Created by Stan on 8/27/15.
+//  Copyright (c) 2015 Ably. All rights reserved.
+//
+
+import Foundation
+import JSQMessagesViewController
+
+class ChatViewController: JSQMessagesViewController {
+}
--- demo-ios/EditNameViewController.swift
+++ demo-ios/EditNameViewController.swift
@@ -12,11 +12,26 @@ import ably
 
 class EditNameViewController: UIViewController {
 
+    @IBOutlet weak var nameTextField: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
 
+    override func shouldPerformSegueWithIdentifier(identifier: String?, sender: AnyObject?) -> Bool {
+        return !nameTextField.text.isEmpty
+    }
+    
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        let clientId = nameTextField.text
+        let chatViewController = segue.destinationViewController as! ChatViewController;
+        
+        // JSQMessagesViewController requires properly set senderId and senderDisplayName
+        chatViewController.senderId = clientId
+        chatViewController.senderDisplayName = clientId
+    }
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
